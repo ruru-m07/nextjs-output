6d3ee4c481ff2149971d099bca21f323ed76dcce
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-06-29 13:15:15 +0530

    fix(ui): fix a component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,22 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/app/utils/cn";
-import Link from "next/link";
+
+export const NavbarDemo = () => {
+  return (
+    <div className="relative w-full flex items-center justify-center">
+      <Navbar className="top-2" />
+    </div>
+  );
+}
 
 function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
-    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
       <Menu setActive={setActive}>
         <MenuItem setActive={setActive} active={active} item="Services">
           <div className="flex flex-col space-y-4 text-sm">
@@ -18,7 +27,7 @@ function Navbar({ className }: { className?: string }) {
           </div>
         </MenuItem>
         <MenuItem setActive={setActive} active={active} item="Products">
-          <div className="text-sm grid grid-cols-2 gap-10 p-4">
+          <div className="  text-sm grid grid-cols-2 gap-10 p-4">
             <ProductItem
               title="Algochurn"
               href="https://algochurn.com"
@@ -57,5 +66,3 @@ function Navbar({ className }: { className?: string }) {
     </div>
   );
 }
-
-export default Navbar;
